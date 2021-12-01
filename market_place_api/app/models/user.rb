02d644e8cb5769897a2a1454f1d700d87fce8b35
Jacó Magalhães <jacomagalhaes@gmail.com>
2021-12-01 00:20:04 -0300

    Gemfile

---
+++ market_place_api/app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    validates :email, uniqueness: true
+    validates_format_of :email, with: /@/
+    has_secure_password
+end
---
+++ market_place_api/test/controllers/api/v1/users_controller_test.rb
@@ -0,0 +1,35 @@
+require "test_helper"
+
+class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  setup do
+    @user = users(:one)
+  end
+
+  test "should destroy user" do
+    assert_difference('User.count', -1) do
+      delete api_v1_user_url(@user), as: :json
+    end
+    assert_response :no_content
+  end
+
+  test "should show user" do
+    get api_v1_user_url(@user), as: :json
+    assert_response :success
+    
+    json_response = JSON.parse(self.response.body)
+    assert_equal @user.email, json_response['email']
+  end
+
+  test "should update user" do
+    patch api_v1_user_url(@user), params: { user: { email: @user.email, password: '123456' } }, as: :json
+    assert_response :success
+  end
+
+  test "should not update user when invalid params are sent" do
+    patch api_v1_user_url(@user), params: { user: { email: 'bad_email', password: '123456' } }, as: :json
+    assert_response :unprocessable_entity
+  end
+end
