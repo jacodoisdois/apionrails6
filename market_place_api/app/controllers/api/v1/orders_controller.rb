872996988abe6a9a9b7479c36cf5bd5b8b1c0570
Jacó Magalhães <jacomagalhaes@gmail.com>
2021-12-04 23:11:21 -0300

    Show action for order

--- market_place_api/app/controllers/api/v1/orders_controller.rb
+++ market_place_api/app/controllers/api/v1/orders_controller.rb
@@ -5,4 +5,16 @@ class Api::V1::OrdersController < ApplicationController
     render json: OrderSerializer.new(current_user.orders).serializable_hash
   end
 
+  def show
+    order = current_user.orders.find(params[:id])
+
+    if order
+      options = { include: [:products] }
+      render json: OrderSerializer.new(order, options).serializable_hash
+    else 
+      head 404
+    end
+    
+  end
+
 end
