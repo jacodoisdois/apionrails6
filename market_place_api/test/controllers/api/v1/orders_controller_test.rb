d906cb8ac738d9c3d45007990f8bbc87116f8cfb
Jacó Magalhães <jacomagalhaes@gmail.com>
2021-12-05 19:11:52 -0300

    Allows the order to be placed along with product quantity

--- market_place_api/test/controllers/api/v1/orders_controller_test.rb
+++ market_place_api/test/controllers/api/v1/orders_controller_test.rb
@@ -3,11 +3,26 @@ require "test_helper"
 class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
   setup do 
     @order = orders(:one)
+    @order_params = {
+      order: {
+        product_ids_and_quantities: [
+          { product_id: products(:one).id, quantity: 2 },
+          { product_id: products(:two).id, quantity: 3 },
+        ]
+      }
+    }
+  end
 
-    @order_params = { order: {
-      product_ids: [products(:one).id, products(:two).id],
-      total: 50
-    } }
+  test 'should create order with two products and placements' do
+    assert_difference('Order.count', 1) do
+      assert_difference('Placement.count', 2) do
+        post api_v1_orders_url, 
+        params: @order_params,
+        headers: { Authorization: JsonWebToken.encode(user_id: @order.user_id) },
+        as: :json
+      end
+    end
+    assert_response :created
   end
 
 
@@ -57,5 +72,7 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
     assert_equal @order.products.first.title, include_product_attr['title']
 
   end
+
+
 
 end
