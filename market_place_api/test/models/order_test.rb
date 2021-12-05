b25c7a8306ffb0461e89a6c54b7d4259b0324051
Jacó Magalhães <jacomagalhaes@gmail.com>
2021-12-05 19:30:06 -0300

    Delete total test

--- market_place_api/test/models/order_test.rb
+++ market_place_api/test/models/order_test.rb
@@ -8,15 +8,6 @@ class OrderTest < ActiveSupport::TestCase
     @product2 = products(:two)
   end
 
-  test 'should set total' do
-    order = Order.new user_id: @order.user_id
-    order.products << products(:one)
-    order.products << products(:two)
-    order.save
-
-    assert_equal (@product1.price + @product2.price), order.total
-  end
-
   test 'builds 2 placements for the order' do
     @order.build_placements_with_product_ids_and_quantities([
     {product_id: @product1.id, quantity:2},
--- market_place_api/test/models/placement_test.rb
+++ market_place_api/test/models/placement_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class PlacementTest < ActiveSupport::TestCase
   setup do
-    @placement = @placements(:one)
+    @placement = placements(:one)
   end
 
   test 'decreases the product quantity by the placement quantity' do
