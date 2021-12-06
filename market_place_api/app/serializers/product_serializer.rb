3d8ed72583f11bc20b0b2e8f10897aacdde49237
Jacó Magalhães <jacomagalhaes@gmail.com>
2021-12-05 21:02:21 -0300

    Adds caching for the serializers

--- market_place_api/app/serializers/product_serializer.rb
+++ market_place_api/app/serializers/product_serializer.rb
@@ -2,4 +2,5 @@ class ProductSerializer
   include FastJsonapi::ObjectSerializer
   attributes :title, :price, :published
   belongs_to :user
+  cache_options store: Rails.cache, namespace: 'jsonapi-serializer', expires_in: 1.hour
 end
